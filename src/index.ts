a9464c6b10bcf556ae2d6ea62965da47e2e8bbdd
Fernando Colmenarez Oronoz <fcolmenarez@technisys.com>
2022-04-27 19:50:16 -0400

    14 class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,35 @@
-import {Observable} from 'rxjs';
+import {Observable, Observer} from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('next from observer'),
+    error: err => console.error('error observer',err),
+    complete: () => console.info('Completed from observer')
+}
 
 //const obs$ = Observable.create() --->example
 
 const obs$ = new Observable<string>(subs => {
     subs.next('Hello');
-    subs.next('World')
+    subs.next('World');
+    subs.next('Hi');
+    subs.next('World');
     subs.complete(); // finish  process
 });
 
+// https://rxjs.dev/deprecations/subscribe-arguments
+/*obs$.subscribe(
+    value => console.log('next', value),
+    error => console.warn('error',error),
+    ()=> console.info('completed'),
+)*/
 
-obs$.subscribe(console.log)
+// solved code deprecation
+/*obs$.subscribe({
+        next: (next) => console.log('next', next),
+        error: (err) => console.log('error', err),
+        complete: () => console.log('Complete')
+    }
+);*/
 
+obs$.subscribe(observer);
 
